76f65bab38acd1d71e2763726b76909fa20b424a
Michu-dev <michalciesielski2000@gmail.com>
2023-01-20 14:08:50 +0000

    Overwrite variable

--- Library/datestamp/datestamp.py
+++ Library/datestamp/datestamp.py
@@ -5,7 +5,7 @@ def generate_date(spark, read_path, write_path):
 
     existing_records = spark.read.options(header='True').csv(read_path)
 
-    existing_records.withColumn("datestamp", lit(datetime.now()))
+    existing_records = existing_records.withColumn("datestamp", lit(datetime.now()))
 
     existing_records.write.options(header='True').format("csv").save(write_path)
 
